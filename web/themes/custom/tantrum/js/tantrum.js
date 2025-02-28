7b9a44b4430f1aeb08050b3c497accbb14a92940
salvador.perez <salvapere@gmail.com>
2025-02-28 12:48:54 +0100

    Initial commit

---
+++ web/themes/custom/tantrum/js/tantrum.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * tantrum behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.tantrum = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
